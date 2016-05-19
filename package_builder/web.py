d9117bec7f0a101afac25b4a3aad9076cb9fc6d9
root <root@centos7.localhost>
2016-05-19 17:40:44 -0400

    Now we can download rpms. Added link to search for docker hub container images.

--- package_builder/web.py
+++ package_builder/web.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template,request,session
+from flask import Flask,render_template,request,session,Response
 from subprocess import Popen
 import sys
 import os
 import time
 import package_builder
-from werkzeug import secure_filename
+import tarfile
 
 app = Flask(__name__)    
 
@@ -21,7 +21,7 @@ def index():
 
 @app.route('/build', methods=['POST'])
 def build():
-    if session['sess_num'] == '':
+    if not session.has_key('sess_num'):
         session['sess_num'] = str(time.time()).replace(".","")
     web_upload_files = "web_upload_files_%s" % session['sess_num']
     if not os.path.exists(web_upload_files):
@@ -43,6 +43,20 @@ def test():
     p = Popen(['/root/gotty -w -p 8888 --once package_builder/package_builder.py -t -i '+ session['image'] +' -n '+ session['sess_num'] +' && docker stop package-builder'], shell=True)
     time.sleep(1)
     return render_template('test.html', url="http://192.168.56.101:8888/", sess_num=session['sess_num'])
+
+@app.route('/download_rpms', methods=['GET'])
+def download_rpms():
+    if session['sess_num'] == '':
+        session['sess_num'] = str(time.time()).replace(".","")
+    pb_temp_dir = "package_builder_tmp%s" % session['sess_num']
+    rpm_dir = "./%s/rpmbuild/RPMS" % pb_temp_dir
+    if not os.path.exists(rpm_dir):
+        return render_template('index.html', error="Error: We could not find the %s directory. Please verify if you have build rpm before" % rpm_dir)
+    with tarfile.open("%s/rpms.tar" % pb_temp_dir, "w:gz") as tar:
+        tar.add(rpm_dir, arcname=os.path.basename(rpm_dir))
+    #return render_template('%s/rpms.tar' % pb_temp_dir, sess_num=session['sess_num'])
+    rpm_tar = open("%s/rpms.tar" % pb_temp_dir).read()
+    return Response(rpm_tar, mimetype="application/x-tar")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
